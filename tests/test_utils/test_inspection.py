f13cb8c4e85f41b08b5c88311291ec774596774a
friedkeenan <friedkeenan@protonmail.com>
2021-05-21 01:04:33 -0500

    If this doesn't work I'm dropping support for 3.7

--- tests/test_utils/test_inspection.py
+++ tests/test_utils/test_inspection.py
@@ -1,4 +1,5 @@
 import sys
+import inspect
 import pytest
 from pak import *
 
@@ -8,6 +9,19 @@ def test_arg_annoations():
 
     def test_keyword_required(x, y, *, z):
         pass
+
+    def test_positional_only(x, y, z):
+        pass
+
+    # Python 3.7 can't define positional-only arguments
+    # in pure python, so we manipulate the signature instead.
+    test_positional_only.__signature__ = inspect.Signature(
+        parameters = [
+            inspect.Parameter("x", kind=inspect.Parameter.POSITIONAL_ONLY),
+            inspect.Parameter("y", kind=inspect.Parameter.POSITIONAL_OR_KEYWORD),
+            inspect.Parameter("z", kind=inspect.Parameter.POSITIONAL_OR_KEYWORD),
+        ],
+    )
 
     def test_var_args(x: 1, y: 2, *args: 3, z: 4):
         pass
@@ -27,14 +41,8 @@ def test_arg_annoations():
     with pytest.raises(TypeError, match="Too many positional"):
         util.arg_annotations(test_keyword_required, 1, 2, 3)
 
-    # Python 3.7 can't define positonal arguments
-    # in pure python.
-    if sys.version_info.minor > 7:
-        def test_positional_only(x, /, y, z):
-            pass
-
-        with pytest.raises(TypeError, match="Positional only"):
-            util.arg_annotations(test_positional_only, x=1, y=2, z=3)
+    with pytest.raises(TypeError, match="Positional only"):
+        util.arg_annotations(test_positional_only, x=1, y=2, z=3)
 
     args_annotations, kwargs_annotations = util.arg_annotations(test_var_args, 1, 2, 3, 4, z=5)
     assert len(args_annotations)   == 4
