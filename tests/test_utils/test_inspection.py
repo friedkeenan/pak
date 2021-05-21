f0c8aa699becc7b35b01ec709fc4977094206797
friedkeenan <friedkeenan@protonmail.com>
2021-05-21 00:57:44 -0500

    Hopefully fix tests for python 3.7

--- tests/test_utils/test_inspection.py
+++ tests/test_utils/test_inspection.py
@@ -1,3 +1,4 @@
+import sys
 import pytest
 from pak import *
 
@@ -6,9 +7,6 @@ def test_arg_annoations():
         pass
 
     def test_keyword_required(x, y, *, z):
-        pass
-
-    def test_positional_only(x, /, y, z):
         pass
 
     def test_var_args(x: 1, y: 2, *args: 3, z: 4):
@@ -29,8 +27,14 @@ def test_arg_annoations():
     with pytest.raises(TypeError, match="Too many positional"):
         util.arg_annotations(test_keyword_required, 1, 2, 3)
 
-    with pytest.raises(TypeError, match="Positional only"):
-        util.arg_annotations(test_positional_only, x=1, y=2, z=3)
+    # Python 3.7 can't define positonal arguments
+    # in pure python.
+    if sys.version_info.minor > 7:
+        def test_positional_only(x, /, y, z):
+            pass
+
+        with pytest.raises(TypeError, match="Positional only"):
+            util.arg_annotations(test_positional_only, x=1, y=2, z=3)
 
     args_annotations, kwargs_annotations = util.arg_annotations(test_var_args, 1, 2, 3, 4, z=5)
     assert len(args_annotations)   == 4
