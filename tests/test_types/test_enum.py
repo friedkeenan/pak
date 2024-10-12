17fe7016e81c03f779b271a5dea05eb061e5699a
friedkeenan <friedkeenan@protonmail.com>
2024-10-12 15:39:55 -0500

    Implement asynchronous API for enum types

--- tests/test_types/test_enum.py
+++ tests/test_types/test_enum.py
@@ -6,7 +6,7 @@ class EnumRaw(enum.Enum):
     A = 1
     B = 2
 
-def test_static_enum():
+async def test_static_enum():
     EnumStatic = pak.Enum(pak.Int8, EnumRaw)
 
     pak.test.type_behavior(
@@ -20,17 +20,18 @@ def test_static_enum():
         default     = EnumRaw.A,
     )
 
-    assert EnumStatic.unpack(b"\x03") is pak.Enum.INVALID
+    assert EnumStatic.unpack(b"\x03")             is pak.Enum.INVALID
+    assert await EnumStatic.unpack_async(b"\x03") is pak.Enum.INVALID
 
     assert EnumStatic.size(pak.Enum.INVALID) == 1
 
     with pytest.raises(ValueError, match="invalid value"):
         EnumStatic.pack(pak.Enum.INVALID)
 
-def test_dynamic_enum():
+async def test_dynamic_enum():
     EnumDynamic = pak.Enum(pak.LEB128, EnumRaw)
 
-    pak.test.type_behavior(
+    await pak.test.type_behavior_both(
         EnumDynamic,
 
         (EnumRaw.A, b"\x01"),
@@ -44,7 +45,7 @@ def test_dynamic_enum():
     with pytest.raises(ValueError, match="invalid value"):
         EnumDynamic.size(pak.Enum.INVALID)
 
-test_enum_or = pak.test.type_behavior_func(
+test_enum_or = pak.test.type_behavior_func_both(
     pak.EnumOr(pak.Int8, EnumRaw),
 
     (EnumRaw.A, b"\x01"),
